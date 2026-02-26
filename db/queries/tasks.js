0232d4bfdfb52424b7756f2a9a366be9e90aad30
Kayla Rampersaud <krampersaud98@gmail.com>
2026-02-26 13:58:15 -0500

    set up tasks router

--- db/queries/tasks.js
+++ db/queries/tasks.js
@@ -13,3 +13,48 @@ export async function createTask(title, done, userId) {
   } = await db.query(sql, [title, done, userId]);
   return task;
 }
+
+export async function getTasksByUserId(userId) {
+  const sql = `
+  SELECT *
+  FROM tasks
+  WHERE user_id = $1
+  `;
+  const { rows: tasks } = await db.query(sql, [userId]);
+  return tasks;
+}
+
+export async function getTaskById(id) {
+  const sql = `
+  SELECT *
+  FROM tasks
+  WHERE id = $1
+  `;
+  const {
+    rows: [task],
+  } = await db.query(sql, [id]);
+  return task;
+}
+
+export async function deleteTaskById(id) {
+  const sql = `
+  DELETE FROM tasks
+  WHERE id = $1
+  `;
+  await db.query(sql, [id]);
+}
+
+export async function updateTaskById(id, title, done) {
+  const sql = `
+  UPDATE tasks
+  SET
+    title = $2,
+    done = $3
+  WHERE id = $1
+  RETURNING *
+  `;
+  const {
+    rows: [task],
+  } = await db.query(sql, [id, title, done]);
+  return task;
+}
